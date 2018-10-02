a704c5bfb32dc7c0ae4c1275bf726b387b278e4c
Celia Marshall <celia.m@gmail.com>
2018-10-02 12:08:36 -0700

    made object from new form

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,28 @@ const roommatesContainer = document.querySelector('#roommates')
 roommates.showAll(roommatesContainer)
 
 const newRoommateButton = document.querySelector('#new-roommate-button')
-newRoommateButton.addEventListener('click', () => {
+newRoommateButton.addEventListener('click', (event) => {
+  event.preventDefault()
   const sidebar = document.querySelector('#sidebar')
   roommates.showNewForm(sidebar)
+  
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const newFriend = {
+      'avatar': event.target.avatar.value,
+      'username': event.target.username.value,
+      'faction': event.target.faction.value,
+      'address': {
+        'street': event.target.street.value,
+        'suite': event.target.suite.value,
+        'city': event.target.city.value,
+        'zipcode': event.target.zipcode.value
+      } 
+    }
+    data.push(newFriend)
+
+  })
 })
+
+
